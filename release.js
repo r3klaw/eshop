63b8a74fa7e0ed29a90acdd515880cf58454df8d
r3klaw <rashid.david12@gmail.com>
2019-01-17 13:27:47 +0300

    update jason file

--- release.js
+++ release.js
@@ -19,7 +19,7 @@ const port=process.env.PORT || 3000
 //     res.statusCode = 200;
     // res.setHeader('Content-Type', 'text/html');
     // res.end('<h1>Hello World</h1>');
-});
+// });
 server.listen(port,() => {
     console.log(`Server running at port `+port);
 });
