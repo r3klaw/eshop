6c9233e54a28cfcdbab73e243602c0898aaabc5f
r3klaw <rashid.david12@gmail.com>
2019-01-17 13:26:17 +0300

    update jason file

--- release.js
+++ release.js
@@ -15,10 +15,10 @@ require('total.js').http('release', options);
 // require('total.js').cluster.http(5, 'release', options);
 const http = require('http');
 const port=process.env.PORT || 3000
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    res.end('<h1>Hello World</h1>');
+// const server = http.createServer((req, res) => {
+//     res.statusCode = 200;
+    // res.setHeader('Content-Type', 'text/html');
+    // res.end('<h1>Hello World</h1>');
 });
 server.listen(port,() => {
     console.log(`Server running at port `+port);
